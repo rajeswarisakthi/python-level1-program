53647bb34326cc5f55a9fdf7e640fb54fa4ddc86
rajeswarisakthi <rajeswarisakthivel1307@gmail.com>
2025-07-31 14:44:12 +0530

    py7

---
+++ raji7.py
@@ -0,0 +1,11 @@
+print("Triangle program")
+print("-------------------")
+no1=int(input("Enter the a value:"))
+no2=int(input("Enter the b value:"))
+no3=int(input("Enter the c value:"))
+no4=int(input("Enter the h value:"))        
+print("Result")
+pt=no1+no2+no3
+print("Perimeter of triangle:",pt)
+at=1/2*no2*no4
+print("Area of triangle:",at)        
