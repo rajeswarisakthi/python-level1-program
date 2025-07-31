c4638250e3ea83c5c8dbd77abb493ad88ef988fd
rajeswarisakthi <rajeswarisakthivel1307@gmail.com>
2025-07-31 11:09:50 +0530

    py5

---
+++ raji5.py
@@ -0,0 +1,17 @@
+print("Square of rectangle")
+print("---------------")
+a=int(input("Enter the a value:"))
+b=int(input("Enter the b value:"))
+l=int(input("Enter the l value:"))
+print("Result")
+PS=4*a
+print("Perimeter of square:",PS)
+AS=a*a
+print("Area of square:",AS)
+PR=2*(l+b)
+print("Perimeter of rectangle:",PR)
+AR=l*b
+print("Area of rectangle:",AR)
+      
+
+        
