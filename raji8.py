3bfaefe00d96c7215eab99e8f5310fb58fa82c21
rajeswarisakthi <rajeswarisakthivel1307@gmail.com>
2025-07-31 14:50:11 +0530

    py8

---
+++ raji8.py
@@ -0,0 +1,11 @@
+print("Sum of n number")
+print("--------------------")
+n=int(input("Enter the n value:"))
+print("Result")
+s=n*(n+1)/2
+print("Sum:",s)
+ss=n*(n+1)*(2*n+1)/6
+print("sum square:",ss)
+sc=(n*(n+1)/2)*(n*(n+1)/2)
+print("Sum cube:",sc)      
+      
