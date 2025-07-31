11dd626413f18cdda63bc78ccf7961afcc973e58
rajeswarisakthi <rajeswarisakthivel1307@gmail.com>
2025-07-31 15:10:32 +0530

    py9

---
+++ raji9.py
@@ -0,0 +1,14 @@
+print("Exponential program")
+print("------------------")
+x=int(input("Enter the x value:"))
+y=int(input("Enter the y value:"))
+z=int(input("Enter the z value:"))
+print("Result")
+p=x^2+y^2+z^2
+print("p:",p)
+q=x^3+y^2+z+20
+print("q:",q)
+r=x^2*y+y^2*z+z^2*x+10
+print("r:",r)
+s=x^3*y^2*z+2*x*y*z+x*y*z
+print("s:",s)
