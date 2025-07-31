f0e7a50c3d4052fc68a58d3023672e3ab0e37f80
rajeswarisakthi <rajeswarisakthivel1307@gmail.com>
2025-07-31 14:39:55 +0530

    py6

---
+++ raji6.py
@@ -0,0 +1,8 @@
+print("Circle program")
+print("----------------")
+r=int(input("Enter the r value:"))
+print("result")
+pc=2*22/7*r
+print("perimeter of circle:",pc)
+ac=22/7*r*r
+print("Area of circle:",ac)       
